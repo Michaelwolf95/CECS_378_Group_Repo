e6df84ef7a1e268e4f45cee25950cb5a34825bed
THEALPHAMACHINE\SupaC <chris_meyer_2@hotmail.com>
2018-03-15 00:30:05 -0700

    Possibly done iteration

--- File_Encryption/CECS_378_File_Encryption.py
+++ File_Encryption/CECS_378_File_Encryption.py
@@ -1,166 +1,262 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  6 12:43:47 2018
-
-CECS 378 Project - File Encryption
-
-@author: Michael Wolf & Chris Meyer
-"""
-
-"""
-In this phase, you'll develop modules that will encrypt/decrypt a file.
-
-I recommend using Python Cryptography (hazmat ONLY!). If you decide to use JS, there is vanilla JS lib at here. 
-Should you have any questions regarding the crypto building blocks then do not hesitate to ask the instructor.
-
-Make sure to use github to commit and push all of your code so the instructor can see your source.
-
-You will design these modules:
-"""
-# IMPORTS
 import os
+import sys
+import json
+import base64
 
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.ciphers import (
     Cipher, algorithms, modes
 )
-
-'''
-#Code that we may use as a reference for syntax
-backend = default_backend()
-key = os.urandom(32)
-iv = os.urandom(16)
-cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-encryptor = cipher.encryptor()
-ct = encryptor.update(b"a secret message") + encryptor.finalize()
-decryptor = cipher.decryptor()
-decryptor.update(ct) + decryptor.finalize()
-'''
-keyLength = 32
+#Length values for key and IV
 IVLength = 16
-while(keyLength != 32):
-    keyLength = int(input("Key must be 32 bytes, please enter in correct key length"))
+KeyLength = 32
+#Encryption of data
+def Myencrypt(plaintext, key):
+  #Generates an iv of a given length
+  iv = os.urandom(IVLength)
+  #Creates an encryptor in the specified mode with the given ivs and key
+  encryptor = Cipher(
+      algorithms.AES(key),
+      modes.GCM(iv),
+      backend=default_backend()
+  ).encryptor()
+  #Encrypts the plaintext
+  ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+  #returns the values gotten
+  return (iv, ciphertext, encryptor.tag)
     
-#Generates a random key
-key = os.urandom(keyLength)
+def MyfileEncrypt(filepath):
+  #Generates the key
+  key = os.urandom(KeyLength)
+  #Retrieves the file extension
+  file_name, file_extension = os.path.splitext(filepath)
 
-def MyEncrypt(key, plaintext, associated_data):
-    # Generate a random 96-bit IV.
-    iv = GenerateIV()
+  #Opens the file specified at the file path in mode read binary
+  with open(filepath, "rb") as binary_file:
+    # Read the whole file at once
+    data = binary_file.read()
+    #Encrypts the file
+    iv, ciphertext, tag = Myencrypt(
+      data,
+      key
+    )
+    #Returns the encrypted values
+    return (ciphertext, tag, iv, key, file_extension)
 
-    # Construct an AES-GCM Cipher object with the given key and a
-    # randomly generated IV.
-    encryptor = Cipher(
-        algorithms.AES(key),
-        modes.GCM(iv),
-        backend=default_backend()
-    ).encryptor()
+def Mydecrypt(ciphertext, tag, iv, key):
+  #Decrypts the ciphertext using the tag, iv and key
+  decryptor = Cipher(
+      algorithms.AES(key),
+      modes.GCM(iv, tag),
+      backend=default_backend()
+  ).decryptor()
+  #Returns the decrypted value
+  return decryptor.update(ciphertext) + decryptor.finalize()
 
-    # associated_data will be authenticated but not encrypted,
-    # it must also be passed in on decryption.
-    encryptor.authenticate_additional_data(associated_data)
-
-    # Encrypt the plaintext and get the associated ciphertext.
-    # GCM does not require padding.
-    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
-
-    return (iv, ciphertext, encryptor.tag)
+def MyfileDecrypt(filepath, key):
+  #Retrieves the name of the file from the given file path without extension
+  file_name = os.path.splitext(filepath)[0]
+  
+  #Opens file and reads the data as a json file format
+  with open(filepath, 'r') as f:
+  #Loads the json data format that was passed in from encryption
+    data = json.load(f)
     
-def MyDecrypt(key, associated_data, iv, ciphertext, tag):
-    # Construct a Cipher object, with the key, iv, and additionally the
-    # GCM tag used for authenticating the message.
-    decryptor = Cipher(
-        algorithms.AES(key),
-        modes.GCM(iv, tag),
-        backend=default_backend()
-    ).decryptor()
+    #Decodes the data from base 64 value of the iv, ciphertext and tag
+    iv = base64.b64decode(data['iv'])
+    ciphertext = base64.b64decode(data['ciphertext_base64'])
+    tag = base64.b64decode(data['tag'])
+    #Calls the mydecrypt method of the plaintext
+    plaintext = Mydecrypt(ciphertext, tag, iv, key)
+    #Creates the file name and extensino that was stored
+    output_filename = file_name + data['file_extension']
+    #Writes back to the file in binary specified by the file name
+    f = open(output_filename, 'wb')
+    f.write(plaintext)
+    f.close()
+    #returns the name of the decrypted file
+    return output_filename
 
-    # We put associated_data back in or the tag will fail to verify
-    # when we finalize the decryptor.
-    decryptor.authenticate_additional_data(associated_data)
-
-    # Decryption gets us the authenticated plaintext.
-    # If the tag does not match an InvalidTag exception will be raised.
-    return decryptor.update(ciphertext) + decryptor.finalize()
-
-iv, ciphertext, tag = MyEncrypt (
+def encrypt_file(filepath):
+  #Retrieves the file name from the file path
+  file_name = os.path.splitext(filepath)[0]
+  #encrypts the file at the given filepath
+  ciphertext, tag, iv, key, file_extension = MyfileEncrypt(filepath)
+  #Creates an empty dictionary
+  data = {}
+  #Sets the dictionary with the corresponding values
+  #The values are encoded in base64 so that there is no data loss across file translation
+  data['ciphertext_base64'] = base64.b64encode(ciphertext).decode('utf-8')
+  data['tag'] = base64.b64encode(tag).decode('utf-8')
+  data['iv'] = base64.b64encode(iv).decode('utf-8')
+  data['file_extension'] = file_extension
+  
+  #Creates the new name for the file using the original file name with a customized ext
+  output_filename = file_name + '.mycrypt'
+  
+  #Creates a new file using the file name
+  with open(output_filename, 'w') as outfile:
+    #writes to the file using json format for the data dictionary to be retrieved during decryption
+    outfile.write(json.dumps(data, outfile))
+    #returns the key and the new file name
+    return (key, output_filename)
+    
+def MyRSAEncrypt(filepath, RSA_PublicKey_filepath):
+  #Calls my file encrypt to encrypt the file
+  ciphertext, tag, iv, key, file_extension = MyfileEncrypt(filepath)
+  #checks for a password to access the rsa key pair
+  password = input("Input your password for accessing your public key or leave blank if you have none")
+  if password == "":
+      password = None
+  else:
+      password = bytes(password, 'utf-8')
+  #opens the rsa key filepath and reads the private key
+  with open(RSA_PublicKey_filepath, 'rb') as key_file:
+    private_key = serialization.load_pem_private_key(
+      key_file.read(),
+      password,
+      backend=default_backend()
+    )
+  #closes the open file  
+  key_file.close()
+  #retireves the public key from the private retrieved (One thing I'm unsure of)
+  public_key = private_key.public_key()
+  #encrypts the key generated from myfileEncrypt with rsa public key
+  RSACipher = public_key.encrypt(
     key,
-    b"a secret message!",
-    b"authenticated but not encrypted payload"
-)
+    padding.OAEP(
+      mgf=padding.MGF1(algorithm=hashes.SHA256()),
+      algorithm=hashes.SHA256(),
+      label=None
+      )
+    )
+  #returns the values
+  return (RSACipher, ciphertext, tag, iv, file_extension)
+         
+def rsa_encrypt_file(filepath, RSA_PublicKey_filepath):
+  #Retrieves the file name from the file path
+  file_name = os.path.splitext(filepath)[0]
+  #encrypts the file at the given filepath with rsa encryption
+  RSACipher, ciphertext, tag, iv, file_extension = MyRSAEncrypt(filepath, RSA_PublicKey_filepath)
+  #Creates an empty dictionary
+  data = {}
+  #Sets the dictionary with the corresponding values
+  #The values are encoded in base64 so that there is no data loss across file translation
+  data['ciphertext_base64'] = base64.b64encode(ciphertext).decode('utf-8')
+  data['tag'] = base64.b64encode(tag).decode('utf-8')
+  data['iv'] = base64.b64encode(iv).decode('utf-8')
+  data['file_extension'] = file_extension
+  
+  #Creates the new name for the file using the original file name with a customized ext
+  output_filename = file_name + '.rsamycrypt'
+  
+  #Creates a new file using the file name
+  with open(output_filename, 'w') as outfile:
+    #writes to the file using json format for the data dictionary to be retrieved during decryption
+    outfile.write(json.dumps(data, outfile))
+    #returns the key and the new file name
+    return (RSACipher, output_filename)
 
-print(MyDecrypt(
-    key,
-    b"authenticated but not encrypted payload",
-    iv,
-    ciphertext,
-    tag
-))
-
-""" PART 1
-(C, IV)= Myencrypt(message, key):
-
-In this method, you will generate a 16 Bytes IV, and encrypt the message using the key and IV in CBC mode (AES). 
-You return an error if the len(key) < 32 (i.e., the key has to be 32 bytes= 256 bits).
-
-(You'll have to write the inverse of the above methods.)
-"""
-
-def GenerateIV():
-    iv =  os.urandom(IVLength) #random.
-    
-    return iv;
-
-def MyEncrypt(message, key):
-    result = ""
-    iv = 1
-    #random.rand
-    #iv = Hazmat.primitives.ciphers.modes.CBC.initialization_vector
-    print(iv)
-    return result, iv;
-
-
-
-#MyEncrypt("Test", "NotAKey")
-
-def MyDecrypt(message, key, iv):
-    result = ""
-    
-    return result;
-
-#key = Fernet.generate_key()
-#cipher_suite = Fernet(key)
-#encoded_text = cipher_suite.encrypt(b"Hello stackoverflow!")
-#print(encoded_text)
-#decoded_text = cipher_suite.decrypt(encoded_text)
-#print(decoded_text)
-
-""" PART 2
-(C, IV, key, ext)= MyfileEncrypt (filepath):
-
-In this method, you'll generate a 32Byte key. You open and read the file as a string.
-You then call the above method to encrypt your file using the key you generated. 
-You return the cipher C, IV, key and the extension of the file (as a string).
-
-You'll have to write the inverse of the above methods. 
-
-You will be asked to encrypt a JPEG file and then decrypt it and make sure you still can view the image.
-"""
-
-
-
-""" PART 3
-Next, you will be asked to write a method as below:
-
-(RSACipher, C, IV, ext)= MyRSAEncrypt(filepath, RSA_Publickey_filepath):
-
-In this method, you first call MyfileEncrypt (filepath) which will return (C, IV, key, ext). 
-You then will initialize an RSA public key encryption object and load pem publickey from the RSA_publickey_filepath. 
-Lastly, you encrypt the key variable ("key") using the RSA publickey in OAEP padding mode. The result will be RSACipher. 
-You then return (RSACipher, C, IV, ext). Remember to do the inverse (MyRSADecrypt (RSACipher, C, IV, ext, RSA_Privatekey_filepath)) 
-which does the exactly inverse of the above and generate the decrypted file using your previous decryption methods.
-
-Make sure to use github to commit and push all of your code so the instructor can see your source.
-"""
-
-
+def MyfileRSADecrypt(filepath, RSACipher, RSA_Privatekey_filepath):
+  #retrieves the file name from the encrypted file
+  file_name = os.path.splitext(filepath)[0]
+  #Checks for a password to the rsa key file
+  password = input("Input your password for accessing your public key or leave blank if you have none")
+  if password == "":
+      password = None
+  else:
+      password = bytes(password, 'utf-8')  
+  #Reads the private key from the rsa key file
+  with open(RSA_Privatekey_filepath, 'rb') as key_file:
+    private_key = serialization.load_pem_private_key(
+      key_file.read(),
+      password,
+      backend=default_backend()
+    )
+  key_file.close()
+  #Uses the private key to decrypt the RSACipher so that the key to the data can be retrieved
+  key = private_key.decrypt(
+      RSACipher,
+      padding.OAEP(
+          mgf=padding.MGF1(algorithm=hashes.SHA256()),
+          algorithm=hashes.SHA256(),
+          label=None
+          )
+      )  
+  
+  #Opens file and reads the data as a json file format
+  with open(filepath, 'r') as f:
+  #Loads the json data format that was passed in from encryption
+    data = json.load(f)
+    #Decodes the data from base 64 value of the iv, ciphertext and tag
+    iv = base64.b64decode(data['iv'])
+    ciphertext = base64.b64decode(data['ciphertext_base64'])
+    tag = base64.b64decode(data['tag'])
+    #Calls the mydecrypt method of the plaintext
+    plaintext = Mydecrypt(ciphertext, tag, iv, key)
+    #Creates the file name and extensino that was stored
+    output_filename = file_name + data['file_extension']
+    #Writes back to the file in binary specified by the file name
+    f = open(output_filename, 'wb')
+    f.write(plaintext)
+    f.close()
+    #returns the name of the decrypted file
+    return output_filename
+## UI
+#sys.argv is the list of command line arguments when enacting a python script
+#if command '--encrypt' is in the command line argument
+if '--encrypt' in sys.argv:
+  #the file path is the command after the --encrypt command
+  filepath = sys.argv[sys.argv.index('--encrypt') + 1]
+  #calls the encrypt_file on the filepath
+  key, output_filename = encrypt_file(filepath)
+  #removes the current file
+  os.remove(filepath)
+  #prints out the key and the outputted file in base64 so no data is lost
+  print("Key: ", base64.b64encode(key).decode('utf-8'))
+  print("Output file: ", output_filename)
+  #Checks for the --decrypt amd --key command in the command line arguments
+elif '--decrypt' in sys.argv and '--key' in sys.argv:
+  #Checks for the file path in the command following the --decrypt
+  filepath = sys.argv[sys.argv.index('--decrypt') + 1]
+  #Key is decoded from base64 so it works in decrypting the file
+  key = base64.b64decode(sys.argv[sys.argv.index('--key') + 1])
+  #The fileDecrypt method is called
+  output_filename = MyfileDecrypt(filepath, key)
+  #Removes the encrypted file path
+  os.remove(filepath)
+  #prints the name of the outputted file
+  print("Output file: ", output_filename)
+#Checks for --rsaencrypt and --rsakeypath in the command line arguments
+elif '--rsaencrypt' in sys.argv and '--rsakeypath' in sys.argv:
+  #takes in the file to be rsa encrypted
+  filepath = sys.argv[sys.argv.index('--rsaencrypt') + 1]
+  #The rsa public key filepath
+  RSA_PublicKey_filepath = sys.argv[sys.argv.index('--rsakeypath') + 1]
+  #calls the rsa encryption method
+  RSACipher, output_filename = rsa_encrypt_file(filepath, RSA_PublicKey_filepath)
+  #os.remove(filepath)
+  #prints out the key and the outputted file in base64 so no data is lost
+  print("RSACipher: ", base64.b64encode(RSACipher).decode('utf-8'))
+  print("Output file: ", output_filename)
+#Checks for --rsaencrypt and --rsakeypath and --rsacipher in the command line arguments
+elif '--rsadecrypt' in sys.argv and '--rsacipher' in sys.argv and '--rsakeypath' in sys.argv:
+  #takes in the file to be decrypted
+  filepath = sys.argv[sys.argv.index('--rsadecrypt') + 1]
+  #decodes the rsacipher from base64
+  RSACipher = base64.b64decode(sys.argv[sys.argv.index('--rsacipher') + 1])
+  #takes in the file path to the private key
+  RSA_Privatekey_filepath = sys.argv[sys.argv.index('--rsakeypath') + 1]
+  #retrieves the new outputted file name
+  output_filename = MyfileRSADecrypt(filepath, RSACipher, RSA_Privatekey_filepath)
+  #removes the encrypted file
+  os.remove(filepath)
+  #displays the new outputted file
+  print("Output file: ", output_filename)
+#If the command was enacted wrong it will print out the correct way 
+else:
+  print("for regular file encryption\n[--encrypt {filename}] or [--decrypt {filename} --key {key}] is required")
+  print("for rsa file encryption\n[--rsaencrypt {filename} --rsakeypath {keyfilename}] or [--rsadecrypt {filename} --rsakeypath (keyfilename) --rsacipher {rsakey}] is required")
